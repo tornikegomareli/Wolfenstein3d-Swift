15dc75e87b1b03fe3ac383697e053eadf309ae1a
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-05-25 23:29:50 +0400

    feature: texture mapping

--- Wolfenstein3d-Swift-CPU/Engine/Rendering/TextureManager.swift
+++ Wolfenstein3d-Swift-CPU/Engine/Rendering/TextureManager.swift
@@ -1,52 +1,51 @@
-// Engine/Rendering/TextureManager.swift
+//
+//  TextureManager.swift
+//  Wolfenstein3d-Swift-CPU
+//
+//  Created by Tornike Gomareli on 25.05.25.
+//
+
+
 import Foundation
 
 class TextureManager {
-    // MARK: - Singleton
-    
-    static let shared = TextureManager()
-    
-    // MARK: - Properties
-    
-    private var textures: [Int: Texture] = [:]
-    
-    // MARK: - Initialization
-    
-    private init() {
-        loadDefaultTextures()
+  static let shared = TextureManager()
+  
+  private var textures: [Int: Texture] = [:]
+  
+  private init() {
+    loadDefaultTextures()
+  }
+  
+  // MARK: - Texture Loading
+  
+  private func loadDefaultTextures() {
+    /// Load wall textures
+    /// Wall type 1, 3, 5 use wall.png
+    if let wallTexture = Texture(named: "wall") {
+      textures[1] = wallTexture
+      textures[3] = wallTexture
+      textures[5] = wallTexture
+    } else {
+      print("Warning: Could not load wall.png texture")
     }
     
-    // MARK: - Texture Loading
-    
-    private func loadDefaultTextures() {
-        // Load wall textures
-        // Wall type 1, 3, 5 use wall.png
-        if let wallTexture = Texture(named: "wall") {
-            textures[1] = wallTexture
-            textures[3] = wallTexture
-            textures[5] = wallTexture
-        } else {
-            print("Warning: Could not load wall.png texture")
-        }
-        
-        // Wall type 2, 4 use wall2.png
-        if let wall2Texture = Texture(named: "wall2") {
-            textures[2] = wall2Texture
-            textures[4] = wall2Texture
-        } else {
-            print("Warning: Could not load wall2.png texture")
-        }
+    /// Wall type 2, 4 use wall2.png
+    if let wall2Texture = Texture(named: "wall2") {
+      textures[2] = wall2Texture
+      textures[4] = wall2Texture
+    } else {
+      print("Warning: Could not load wall2.png texture")
     }
-    
-    // MARK: - Public Methods
-    
-    func texture(for wallType: Int) -> Texture? {
-        return textures[wallType]
+  }
+  
+  func texture(for wallType: Int) -> Texture? {
+    return textures[wallType]
+  }
+  
+  func loadTexture(named name: String, for wallType: Int) {
+    if let texture = Texture(named: name) {
+      textures[wallType] = texture
     }
-    
-    func loadTexture(named name: String, for wallType: Int) {
-        if let texture = Texture(named: name) {
-            textures[wallType] = texture
-        }
-    }
+  }
 }
